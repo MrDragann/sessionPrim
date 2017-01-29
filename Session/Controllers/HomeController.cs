95e43017784081795efee6ae0764d5e04fb8f667
MrDragan <MrDragan@MRDRAGAN-LP>
2017-01-29 19:33:36 +0200

    приветос

--- Session/Controllers/HomeController.cs
+++ Session/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Session.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -22,11 +24,12 @@ namespace Session.Controllers
             }
             return View();
         }
+        BookContext db = new BookContext();
 
         public ActionResult About()
         {
             
-            return View();
+            return View(db.Books);
         }
 
         public ActionResult Contact()
@@ -39,6 +42,86 @@ namespace Session.Controllers
         {
             Session["SessionText"] = "Данный текст хранится в объекте Session";
             return View();
+        }
+        /// <summary>
+        /// Редактирование модели
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult EditBook(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            Book book = db.Books.Find(id);
+            if (book != null)
+            {
+                return View(book);
+            }
+            return HttpNotFound();
+        }
+
+        [HttpPost]
+        public ActionResult EditBook(Book book)
+        {
+            db.Entry(book).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("About");
+        }
+        /// <summary>
+        /// Добавление модели
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Book book)
+        {
+            db.Books.Add(book);
+            db.SaveChanges();
+            return RedirectToAction("About");
+        }
+        /// <summary>
+        /// Удаление модели
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            Book b = db.Books.Find(id);
+            if (b == null)
+            {
+                return HttpNotFound();
+            }
+            return View(b);
+        }
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Book b = db.Books.Find(id);
+            if (b == null)
+            {
+                return HttpNotFound();
+            }
+            db.Books.Remove(b);
+            db.SaveChanges();
+            return RedirectToAction("About");
+        }
+
+        /// <summary>
+        /// Закрытие подключения
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
